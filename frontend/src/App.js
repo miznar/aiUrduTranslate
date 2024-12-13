8628ca1b002486c7776f4c9d5231d0f284b67648
miznar <2021cs174@student.uet.edu.pk>
2024-12-13 14:09:33 +0500

    ...

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import {Route, Routes,route} from 'react-router-dom'
+import {Route, Routes} from 'react-router-dom'
 import Home from './components/Home';
 import About from './components/About';
 import Search from './components/Search';
